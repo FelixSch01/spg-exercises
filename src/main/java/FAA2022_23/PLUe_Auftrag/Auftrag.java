29059ae03fed6913013ac0f5203026edce63651f
Manoel Tesar <manoel.tesar@gmail.com>
2022-11-06 17:30:21 +0100

    Added getter and setter methods.

--- src/main/java/FAA2022_23/PLUe_Auftrag/Auftrag.java
+++ src/main/java/FAA2022_23/PLUe_Auftrag/Auftrag.java
@@ -5,6 +5,7 @@ public class Auftrag {
     private int volumen; // Volumen in EUR
     private boolean erledigt; // Ob der Auftrag erledigt ist
 
+    // ------- ctor ----------
     public Auftrag() {
         ziel = "nicht bekannt";
         volumen = 0;
@@ -15,6 +16,40 @@ public class Auftrag {
         this.ziel = ziel;
         this.volumen = volumen;
         this.erledigt = erledigt;
+    }
+
+    // ------- setter -------
+    public void setZiel(String ziel) {
+        if(ziel != null) {
+            this.ziel = ziel;
+        }
+        else {
+            System.out.println("Fehler bei setZiel(): Wert darf nicht null sein.");
+            this.ziel = "nicht bekannt";
+        }
+    }
+    public void setVolumen(int volumen) {
+        if(volumen >= 0 && volumen <= 10000) {
+            this.volumen = volumen;
+        }
+        else {
+            System.out.println("Fehler bei setVolumen(): Wert muss mindestens 0 und darf maximal 10000 sein.");
+            this.volumen = 0;
+        }
+    }
+    public void setErledigt(boolean erledigt) {
+        this.erledigt = erledigt;
+    }
+
+    // ----- getter ------
+    public String getZiel() {
+        return ziel;
+    }
+    public int getVolumen() {
+        return volumen;
+    }
+    public boolean isErledigt() {
+        return erledigt;
     }
 
     public void printAuftrag() {
