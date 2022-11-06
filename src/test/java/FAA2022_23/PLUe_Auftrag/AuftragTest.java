bad59190611c952405ace00deb492561cfe6bbf4
Manoel Tesar <manoel.tesar@gmail.com>
2022-11-06 18:27:11 +0100

    Wrote first test.

--- src/test/java/FAA2022_23/PLUe_Auftrag/AuftragTest.java
+++ src/test/java/FAA2022_23/PLUe_Auftrag/AuftragTest.java
@@ -1,7 +1,29 @@
 package FAA2022_23.PLUe_Auftrag;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuftragTest {
+    @Test
+    @DisplayName("Test setZiel")
+    public void testSetZiel() {
+        Auftrag a1 = new Auftrag();
+        Auftrag a2 = new Auftrag();
+        Auftrag a3 = new Auftrag();
+
+        a1.setZiel("test");
+        a2.setZiel(null);
+        a3.setZiel("");
+
+        assertAll(() -> assertEquals("test", a1.getZiel()),
+                () -> assertEquals("nicht bekannt", a2.getZiel()),
+                () -> assertEquals("", a3.getZiel()));
+
+
+    }
+
+
 
 }
