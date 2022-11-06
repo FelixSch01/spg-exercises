1d7d0f79c0aa204b86b6826ac6d0d1bb134a9dae
Felix Schmölzer <felix.schmoelzer1@gmail.com>
2022-11-06 17:11:52 +0100

    implement printAuftrag()

--- src/main/java/FAA2022_23/PLUe_Auftrag/Auftrag.java
+++ src/main/java/FAA2022_23/PLUe_Auftrag/Auftrag.java
@@ -1,4 +1,13 @@
 package FAA2022_23.PLUe_Auftrag;
 
 public class Auftrag {
+    public void printAuftrag() {
+        System.out.println(String.format("""
+                Auftragsinfos
+                =============
+                Ziel: %s
+                Volumen: %d
+                Erledigt: %
+                """, ziel, volumen, erledigt));
+    }
 }
